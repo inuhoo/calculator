fe15aa1217c3a6a684f67e0274daf3fb42eca42b
Adam Snape <adam@snape.xyz>
2018-06-15 14:50:54 +0100

    add basic display functions and button listeners

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,14 @@
+// globals 
+const digits = Array.from(document.querySelectorAll('.digit')); //select all the number keys 
+const operators = Array.from(document.querySelectorAll('.operator')); //select all the number keys
+const equals = document.querySelector('#equals')
+const mainDisplay = document.querySelector('#main'); 
+const subDisplay = document.querySelector('#sub'); 
+
+
+// OPERATIONS 
+// ----------
+
 // basic operators
 function add(a,b) {
     return a + b;
@@ -16,16 +27,62 @@ function multiply(a,b) {
 }
 
 // master operate function
-function operate(a, operator, b) {
+function operate() {
     switch (operator) {
         case 'x':
-            return multiply(a, b);
+            return multiply(nums[0], nums[1]);
         case '/':
-            return divide(a,b);
+            return divide(nums[0], nums[1]);
         case '-':
-            return minus(a, b);
+            return minus(nums[0], nums[1]);
         case '+':
-            return add(a, b);
+            return add(nums[0], nums[1]);
     }
 }
 
+
+
+// EVENTS
+// ------
+
+
+
+// add click events to digit buttons
+for (i = 0; i < digits.length; i++) {
+    digits[i].addEventListener('click' , e => {
+        mainDisplay.innerHTML += e.target.value; 
+    });
+};
+
+// add click events to operator buttons
+for (i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click' , e => {
+        subDisplay.innerHTML = mainDisplay.innerHTML + " " + e.target.value;
+        mainDisplay.innerHTML = "";
+    });
+};
+
+// run operate function on equals press
+equals.addEventListener('click', e => {
+});
+
+
+
+// CLEAR FUNCTIONS 
+// -----------------
+
+
+//clear all button press
+document.querySelector("[value='clr-all']").addEventListener('click', e => {
+    subDisplay.innerHTML = "" ; 
+    mainDisplay.innerHTML = "";
+});
+
+//clear button press
+document.querySelector("[value='clr']").addEventListener('click', e => {
+    mainDisplay.innerHTML = "";
+});
+//clear button press
+document.querySelector("[value='bksp']").addEventListener('click', e => {
+    mainDisplay.innerHTML = mainDisplay.innerHTML.slice(0, -1);
+});
