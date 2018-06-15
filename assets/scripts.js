8fd0ddcf9b24ffe085831580dbeefdc6497b2e15
Adam Snape <adam@snape.xyz>
2018-06-16 00:19:52 +0100

    Remove spaghetti logic, start over

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,84 +1,81 @@
-// globals 
-const digits = Array.from(document.querySelectorAll('.digit')); //select all the number keys 
+// globals
+const digits = Array.from(document.querySelectorAll('.digit')); //select all the number keys
 const operators = Array.from(document.querySelectorAll('.operator')); //select all the number keys
 const equals = document.querySelector('#equals')
-const mainDisplay = document.querySelector('#main'); 
-const subDisplay = document.querySelector('#sub'); 
+const mainDisplay = document.querySelector('#main');
+const subDisplay = document.querySelector('#sub');
+let operands = [];
+let operation;
+let solution;
+let reset = 0;
 
-
-// OPERATIONS 
+// OPERATIONS
 // ----------
 
 // basic operators
-function add(a,b) {
-    return a + b;
-}
-
-function minus(a, b) {
-    return a - b;
-}
-
-function divide(a,b) {
-    return (b === 0 ? "Oops there it goes" : a / b);
-}
-
-function multiply(a,b) {
-    return a * b;
-}
+function add(a,b) {return a + b;}
+function minus(a,b) {return a - b;}
+function divide(a,b) {return (b === 0 ? "Oops there it goes" : a / b);}
+function multiply(a,b) {return a * b;}
 
 // master operate function
-function operate() {
-    switch (operator) {
-        case 'x':
-            return multiply(nums[0], nums[1]);
-        case '/':
-            return divide(nums[0], nums[1]);
-        case '-':
-            return minus(nums[0], nums[1]);
-        case '+':
-            return add(nums[0], nums[1]);
+function operate(nums, o) {
+    subDisplay.innerHTML = operands[0] + " " + operation + " " + operands[1]
+    switch (o) {
+        case 'x': return multiply(nums[0], nums[1]);
+        case '/': return divide(nums[0], nums[1]);
+        case '-': return minus(nums[0], nums[1]);
+        case '+': return add(nums[0], nums[1]);
     }
 }
 
-// run operate function on equals press
-equals.addEventListener('click', e => {
-    operate();
-});
-
-
 // add digit number to the display
-function updateDisplay(num) {    
+function updateDisplay(num) {
+    if (reset){mainDisplay.innerHTML = ""};
     if (num == "." && /\./.test(mainDisplay.innerHTML)) {
         return;
     }
     mainDisplay.innerHTML += num;
+    reset = 0; // turn off display reset
 }
 
+// update displays when operator is selected
+function operationSelect(op) {};
+
+
+// EVENT LISTENERS
+// ---------------
+
+// add event listeners for digit buttons
 for (i = 0; i < digits.length; i++) {
     digits[i].addEventListener('click' , e => {
-        updateDisplay(e.target.value); 
+        updateDisplay(e.target.value);
     });
 };
 
-// update displays when operator is selected
-function operationSelect(op) {
-    subDisplay.innerHTML = mainDisplay.innerHTML + " " + op;
-    mainDisplay.innerHTML = "";
-}
-
+// add event listeners for operator buttons
 for (i = 0; i < operators.length; i++) {
     operators[i].addEventListener('click' , e => {
         operationSelect(e.target.value);
     });
 };
 
+// event listener for equals button
+equals.addEventListener('click', e => {
+    mainDisplay.innerHTML = operate(operands, operation);
+    operands = [];
+    reset = 1; // toggle display reset
+});
 
 
-// CLEAR FUNCTIONS 
+
+// CLEAR FUNCTIONS
 // -----------------
 function clearAll() {
-    subDisplay.innerHTML = "" ; 
+    subDisplay.innerHTML = "" ;
     mainDisplay.innerHTML = "";
+    operands = [];
+    operation = "";
 }
 
 //clear all button press
@@ -103,12 +100,13 @@ document.querySelector("[value='bksp']").addEventListener('click', e => {
    backspace();
 });
 
+
+// keyboard support
 document.addEventListener('keyup', e => {
-    console.log(e);
     switch (e.key) {
         case "Backspace":
             backspace();
-            break; 
+            break;
         case "Delete":
             clearAll();
             break;
@@ -124,12 +122,15 @@ document.addEventListener('keyup', e => {
         case "0":
         case ".":
             updateDisplay(e.key);
-            break;    
+            break;
         case "+":
         case "-":
         case "*":
         case "/":
             operationSelect(e.key);
+            break;
+        case "Enter":
+            // missing equals function at the moment.
             break;
     }
 })
