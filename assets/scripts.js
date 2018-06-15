3f932546193287a3d32e56ca5a1fd747cd7a1146
Adam Snape <adam@snape.xyz>
2018-06-15 07:30:07 +0100

    Add basic operator functions

---
+++ assets/scripts.js
@@ -0,0 +1,15 @@
+function add(a,b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+function divide(a,b) {
+    return (b === 0 ? "Oops there it goes" : a / b);
+}
+
+function multiply(a,b) {
+    return a * b;
+}
